8883e49fa25142de040d5e2eed0ebd434f37ca81
Viktor Gubkov <viktor.gubkov@tophouse.fun>
2025-11-11 16:51:02 +0500

    Update grid data, add mock config

--- Assets/_LastWar/Scripts/Runtime/Features/CityBuilder/Presentation/Gameplay/Views/Grid/GridView.cs
+++ Assets/_LastWar/Scripts/Runtime/Features/CityBuilder/Presentation/Gameplay/Views/Grid/GridView.cs
@@ -1,9 +1,38 @@
+using System;
+using System.Collections.Generic;
+using _LastWar.CityBuilder.Repositories.Gameplay.Grid;
 using UnityEngine;
 
 namespace _LastWar.CityBuilder.Presentation.Gameplay.Views
 {
     public class GridView : MonoBehaviour
     {
+        [SerializeField] private GridRepository _gridRepository;
         
+        private List<GameObject> _gridObjects;
+
+        private void Start()
+        {
+            CreateGrid();
+        }
+
+        private void CreateGrid()
+        {
+            _gridObjects = new();
+            
+            for (int i = 0 - 1; i < _gridRepository.GridSize.x; i++)
+            {
+                for (int j = 0 - 1; j < _gridRepository.GridSize.y; j++)
+                {
+                    var grid = _gridRepository.GetGrid();
+                    float positionX = i * grid.GridSize.x;
+                    float positionZ = j * grid.GridSize.y;
+                    
+                    var gridObject = Instantiate(grid.Prefab, new Vector3(positionX, 0, positionZ), Quaternion.identity, transform);
+                    grid.name = $"Grid_{i}_{j}";
+                    _gridObjects.Add(gridObject);
+                }
+            }
+        }
     }
 }
--- Assets/_LastWar/Scripts/Runtime/Features/CityBuilder/Repositories/Gameplay/Grid/GridData.cs
+++ Assets/_LastWar/Scripts/Runtime/Features/CityBuilder/Repositories/Gameplay/Grid/GridData.cs
@@ -1,10 +1,17 @@
+using _LastWar.CityBuilder.Domain.Gameplay.Models.Grid;
 using UnityEngine;
 
 namespace _LastWar.CityBuilder.Repositories.Gameplay.Grid
 {
-    [CreateAssetMenu (fileName = "GridData", menuName = "Cities/GridData")]
+    [CreateAssetMenu (fileName = "GridData", menuName = "City Builder/GridData")]
     public class GridData : ScriptableObject
     {
+        [SerializeField] private GridType _gridType;
+        [SerializeField] private Vector2 _gridSize;
         [SerializeField] private GameObject _prefab;
+
+        public GridType GridType => _gridType;
+        public Vector2 GridSize => _gridSize;
+        public GameObject Prefab => _prefab;
     }
 }
