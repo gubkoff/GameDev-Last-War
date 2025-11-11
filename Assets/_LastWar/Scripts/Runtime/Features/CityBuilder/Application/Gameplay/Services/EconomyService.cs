407b553881c32b97335c23799f5cd8e944297197
Viktor Gubkov <viktor.gubkov@tophouse.fun>
2025-11-11 17:00:50 +0500

    Add mock classes. Add asmdef

---
+++ Assets/_LastWar/Scripts/Runtime/Features/CityBuilder/Application/Gameplay/Services/EconomyService.cs
@@ -0,0 +1,7 @@
+namespace _LastWar.CityBuilder.Application.Gameplay.Services
+{
+    public class EconomyService
+    {
+        
+    }
+}
---
+++ Assets/_LastWar/Scripts/Runtime/Features/CityBuilder/Application/Gameplay/UseCases/PlaceBuildingUseCase.cs
@@ -0,0 +1,7 @@
+namespace _LastWar.CityBuilder.Application.Gameplay.UseCases
+{
+    public class PlaceBuildingUseCase
+    {
+        
+    }
+}
